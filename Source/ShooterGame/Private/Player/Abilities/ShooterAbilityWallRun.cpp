72441a8c1dd07c047ee2dfc8ca32054588e13d44
Shred0 <omardellamazza@gmail.com>
2023-05-25 02:23:21 +0200

    implemented base of wall run ability

--- Source/ShooterGame/Private/Player/Abilities/ShooterAbilityWallRun.cpp
+++ Source/ShooterGame/Private/Player/Abilities/ShooterAbilityWallRun.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ShooterGame.h"
+#include "Kismet/KismetMathLibrary.h"
 #include "Player/Abilities/ShooterAbilityWallRun.h"
 
 UShooterAbilityWallRun::UShooterAbilityWallRun():Super()
@@ -37,6 +38,35 @@ UShooterAbilityWallRun::UShooterAbilityWallRun():Super()
 
 int UShooterAbilityWallRun::Effect()
 {
+	AShooterCharacter* Avatar = AbilitySystem->GetShooterAvatar();
+
+	//i can only run on walls in mid-air
+	if (!Avatar->GetMovementComponent()->IsFalling()) {
+		return -1;
+	}
+
+	//i have to stick to a wall from right or left
+	float maxDistance = 30;
+	FVector rightCheckEnd = (Avatar->GetActorRightVector() * maxDistance) + Avatar->GetActorLocation();
+	FVector leftCheckEnd = (Avatar->GetActorRightVector() * -maxDistance) + Avatar->GetActorLocation();
+	//checking right
+	FHitResult HitRightRes;
+	GetWorld()->LineTraceSingleByChannel(HitRightRes, Avatar->GetActorLocation(), rightCheckEnd, ECollisionChannel::ECC_WorldStatic);
+	//checking left
+	FHitResult HitLeftRes;
+	GetWorld()->LineTraceSingleByChannel(HitLeftRes, Avatar->GetActorLocation(), leftCheckEnd, ECollisionChannel::ECC_WorldStatic);
+
+	if (!HitRightRes.bBlockingHit && !HitLeftRes.bBlockingHit) {
+		return -1;
+	}
+
+	if (HitRightRes.bBlockingHit) {
+		//UKismetMathLibrary::rotation;
+		HitRightRes.Normal.Rotation();
+		//Avatar->GetActorRotation(). .Yaw = HitRightRes.Normal.Rotation().Yaw
+		//Avatar->SetActorRotation(HitRightRes.Normal.Rotation().Yaw);
+	}
+
 	return 0;
 }
 
